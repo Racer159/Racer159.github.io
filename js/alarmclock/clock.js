5c27707dfbc1859188276f4c9938d2bf4b476d14
Wayne Starr (Racer159) <racer159@live.com>
2014-11-18 16:49:17 -0800

    changed to use element instead of document

--- js/alarmclock/clock.js
+++ js/alarmclock/clock.js
@@ -11,10 +11,10 @@ app.directive('clock', ['$interval', '$compile', function($interval, $compile) {
 		console.log(element);
 		
 		//get the canvas object
-		var clock = element.find('canvas');
+		var clock = element.find('canvas-' + attrs.size);
 		console.log(clock);
-		console.log(clock);
-		var ctx = clock.getContext('2d');
+		console.log(clock[0]);
+		var ctx = clock[0].getContext('2d');
 
 		//set the alarm variables
 		scope.aonehour = 1;
