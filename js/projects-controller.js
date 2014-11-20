363ac9a68ccd39e951b88a53dc4920fb8b24f189
Wayne Starr (Racer159) <racer159@live.com>
2014-11-19 19:54:09 -0800

    added basic animations

--- js/projects-controller.js
+++ js/projects-controller.js
@@ -1,5 +1,7 @@
 var app = angular.module('main');
 
 app.controller('ProjectsController', function($scope, $log) {
-	
+	$scope.gotoProject = function(project) {
+		$location.path('/projects');
+	};
 });
