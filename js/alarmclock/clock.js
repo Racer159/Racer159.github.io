a7897e9de64d3d44da766dee7c84300d7bbaf9c3
Wayne Starr (Racer159) <racer159@live.com>
2014-11-19 18:33:25 -0800

    changed to a more angular way of representing alarm selects

--- js/alarmclock/clock.js
+++ js/alarmclock/clock.js
@@ -9,6 +9,14 @@ app.directive('clock', ['$interval', '$compile', function($interval, $compile) {
 		var clock = document.getElementById('canvas-' + attrs.size);
 		var ctx = clock.getContext('2d');
 
+		// set the selectable values
+		hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+		minutes = [{value: 0, label: "00"}, {value: 5, label: "05"}, {value: 10, label: "10"}, {value: 15, label: "15"},
+			{value: 20, label: "20"}, {value: 25, label: "25"}, {value: 30, label: "30"}, {value: 35, label: "35"},
+			{value: 40, label: "40"}, {value: 45, label: "45"}, {value: 50, label: "50"}, {value: 55, label: "55"}];
+		ampms = ["AM", "PM"];
+		onoffs = ["ON", "OFF"];
+		
 		//set the alarm variables
 		scope.aonehour = 1;
 		scope.aoneminute = 0;
