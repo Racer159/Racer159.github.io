641dffeac8d50a436a0893da4fda84a29b70d2ac
Wayne Starr <wes7817@g.rit.edu>
2016-02-29 15:05:15 -0800

    Switched to UI-SREF for projects

--- js/projects-controller.js
+++ js/projects-controller.js
@@ -1,7 +1,5 @@
 var app = angular.module('main');
 
 app.controller('ProjectsController', function($scope, $state, $log) {
-	$scope.gotoProject = function(project) {
-		$state.go('.' + project);
-	};
+
 });
